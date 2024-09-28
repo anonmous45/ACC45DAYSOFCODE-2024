848dee766799df572961632aaf9061ce25d0ae52
anonmous45 <46777348+anonmous45@users.noreply.github.com>
2024-09-28 20:49:30 +0530

    Add files via upload

---
+++ Day 1 Easy - Cricket World Cup Qualifier.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int score;
+    scanf("%d", &score);
+    if (score >= 12)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
